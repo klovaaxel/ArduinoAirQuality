3a936fb447071a11bf063bfda79e9e1ae2ccaf26
klovaaxel <klovakarlsson@gmail.com>
2020-04-18 18:02:35 +0200

    Update ArduinoAirQuality.ino
    made scren display temp values and temporarily commented sensor code so it can be run without sensor

--- ArduinoAirQuality/ArduinoAirQuality.ino
+++ ArduinoAirQuality/ArduinoAirQuality.ino
@@ -3,6 +3,10 @@
 #include <MCUFRIEND_kbv.h>   // Hardware-specific library
 MCUFRIEND_kbv tft;           // Declares MCUFRIEND_kbv as tft
 
+#include <Fonts/FreeSans9pt7b.h>
+#include <Fonts/FreeSans12pt7b.h>
+#include <Fonts/FreeSerif12pt7b.h>
+
 // Defines simple colours for screen
 #define BLACK   0x0000
 #define RED     0xF800
@@ -23,35 +27,54 @@ CCS811 myCCS811(CCS811_ADDR);
 void setup() {
   Serial.begin(9600);
 
+  Serial.print("serial started");
   
   // Air quality sensor
-  myCCS811.begin();
-
+  //myCCS811.begin();
+  
   // Screen
   uint16_t ID = tft.readID();
   if (ID == 0xD3) ID = 0x9481;
   tft.begin(ID);
-  tft.setRotation(0);
+  tft.setRotation(2);
+  tft.fillScreen(BLACK);
+
+  Serial.print("setup complete");
 
 }
 
 void loop() {
   // Screen
-  tft.fillScreen(BLACK);
+  showmsgXY(20, 100, 2, &FreeSans12pt7b, "tempCO2");
+  showmsgXY(20, 200, 2, &FreeSans12pt7b, "tempVOC");
+
+  Serial.print("text printed");
 
   
   // Air quality sensor
-  if (myCCS811.dataAvailable())
-  {
-    myCCS811.readAlgorithmResults();
-    int tempCO2 = myCCS811.getCO2();
-    int tempVOC = myCCS811.CCS811::getTVOC();
-  }
-  else if (myCCS811.checkForStatusError())
-  {
-    while(1);
-  }
+//  if (myCCS811.dataAvailable())
+//  {
+//    myCCS811.readAlgorithmResults();
+//    int tempCO2 = myCCS811.getCO2();
+//    int tempVOC = myCCS811.CCS811::getTVOC();
+//  }
+//  else if (myCCS811.checkForStatusError())
+//  {
+//    while(1);
+//  }
+//
+//  delay(1000); //Wait for next reading
+//  
+}
 
-  delay(1000); //Wait for next reading
-  
+void showmsgXY(int x, int y, int sz, const GFXfont *f, const char *msg)
+{
+    int16_t x1, y1;
+    uint16_t wid, ht;
+    tft.setFont(f);
+    tft.setCursor(x, y);
+    tft.setTextColor(WHITE);
+    tft.setTextSize(sz);
+    tft.print(msg);
+    delay(1000);
 }
